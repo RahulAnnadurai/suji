208c33fffeb3ec33e57a6ed9a2a05e7938addfdd
sujipriya7991 <34570207+sujipriya7991@users.noreply.github.com>
2017-12-16 22:28:12 +0530

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,8 +1,11 @@
-def main(a):
-	b=a
-	if(b%2==0):
+a=input("Enter the number")
+if(a.isnumeric()):
+	a=int(a)
+	if(a%2==0):
 		print("the number is even")
 	else:
 		print("the number is odd")
 	
-main(3)
+else:
+	print("invalid")
+	
