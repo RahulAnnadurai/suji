338bc9af658b23185af67b98b04cc68d211f0c48
sujipriya7991 <34570207+sujipriya7991@users.noreply.github.com>
2017-12-17 13:57:03 +0530

    Create s2q4.py

---
+++ s2q4.py
@@ -0,0 +1,19 @@
+a=input()
+b=input()
+odds=[]
+even=[]
+if(a.isnumeric()):
+	a=int(a)
+	if(b.isnumeric()):
+		b=int(b)
+		for i in range(a,b):
+			if(i%2!=0):
+				odds.append(i)
+			else:
+				even.append(i)
+		print(odds)
+		print(even)
+	else:
+		print("invalid")
+else:
+	print("invalid")
